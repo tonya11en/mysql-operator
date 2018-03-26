09eb7634a49196f98bccf25826a5f5ae189ee741
Tony Allen <cyril0allen@gmail.com>
2018-03-26 01:12:16 -0700

    codegen: Remove TODO since dep fixes the problem.

--- codegen.sh
+++ codegen.sh
@@ -2,8 +2,6 @@
 
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# TODO (tallen): Might need to figure out the correct way to find
-#                code-generator.
 cd ${scriptdir}/../../../../vendor/k8s.io/code-generator && ./generate-groups.sh \
   all \
   github.com/tonya11en/mysql-operator/pkg/client \
